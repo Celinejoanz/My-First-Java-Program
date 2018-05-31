b2da3a6fb687b61a6d39bac39399468ab0f8edf7
Chidimma Uba <chidimmajuba@gmail.com>
2018-05-31 14:10:59 -0400

    first commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.company;
+/*
+Multiple
+Line
+Comment
+ */
+public class  HelloWorld {
+
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello, World");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        String name;
+        name = "Alton";
+        System.out.println("My name is " + name);
+    }
+}
